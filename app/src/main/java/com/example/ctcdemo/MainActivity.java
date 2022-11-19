bd116f695b4e2a0cb47c506d4e5f6576d9d5c6f9
Skaum <ht585867@dal.ca>
2022-11-19 16:27:05 -0400

    user class added

--- app/src/main/java/com/example/ctcdemo/MainActivity.java
+++ app/src/main/java/com/example/ctcdemo/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.ctcdemo;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity{
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference();
+
+        Map<String,String> user = new HashMap<>();
+        user.put("Name","Test name");
+        user.put("Email","test@email.com");
+
+        ref.setValue(user);
     }
 }
--- app/src/main/java/com/example/ctcdemo/RegistrationActivity.java
+++ app/src/main/java/com/example/ctcdemo/RegistrationActivity.java
@@ -1,4 +1,5 @@
 package com.example.ctcdemo;
 
 public class RegistrationActivity {
+
 }
